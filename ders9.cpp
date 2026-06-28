3c0d8825466e46e0fa3f38fb37cbf78bbbb9d793
yunusemre-deniz <yunusemre.deniz@outlook.com.tr>
2026-06-28 16:17:00 +0300

    while dongusu

---
+++ ders9.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"dongu basliyor"<<endl;
+
+    int sayac=100;
+
+    while (sayac>0)
+    {
+        cout<<sayac--<<endl;
+
+
+    }
+    cout<<"donguden ciktim"<<endl;
+
+}
