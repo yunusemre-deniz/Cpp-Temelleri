8b1946b7d691f3e87e15d383eb81601b7c386711
yunusemre-deniz <yunusemre.deniz@outlook.com.tr>
2026-06-28 17:27:44 +0300

    aritmetik operatörler

---
+++ ders11.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x;
+    float a,b;
+
+    x=5;
+    a=10;
+    b=7.5;
+
+    x+=a;
+    cout<<x<<endl;
+    b+=a;
+    x*=b;
+
+    cout<<b<<" "<<x;
+}
